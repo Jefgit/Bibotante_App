ac5299c223b305fce142844bf96fa76bf240a27a
Jefgit <riazondajf@gmail.com>
2025-05-09 20:13:13 +0800

    added needed Candidates and Party list data

---
+++ bibotante_backend/src/types.ts
@@ -0,0 +1,12 @@
+export type Candidates = {
+    id: string, 
+    position: string, 
+    firstName: string, 
+    lastName: string, 
+    partyInitials: string
+};
+
+export type PartyList = {
+    id: string,
+    name: string
+};
