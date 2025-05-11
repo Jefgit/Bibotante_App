8287d2fe418cdec36320e66e830153f98b53f497
Jefgit <riazondajf@gmail.com>
2025-05-11 18:18:16 +0800

    rebuild

--- bibotante_frontend/src/constants.ts
+++ bibotante_frontend/src/constants.ts
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = 'https://bibotante-app.onrender.com/api';
-export const apiBaseUrl = 'http://localhost:3000/api';
+export const apiBaseUrl = 'https://bibotante-app.onrender.com/api';
+// export const apiBaseUrl = 'http://localhost:3000/api';
